71d2ebdadcba7887c0f6ac2a24e34d9a4405390e
wallscreet <wallscreet@gmail.com>
2020-06-04 05:21:26 -0500

    fixing summernote issues?

--- RamCoreV3/settings.py
+++ RamCoreV3/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import django_summernote
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'bootstrap4',
     'bootstrap_datepicker_plus',
-    'django_summernote'
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
